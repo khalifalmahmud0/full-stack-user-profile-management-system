759a64b0e5f400d3297c0169789ccfcee3776841
Khalif AL Mahmud <khalif@Khalifs-MacBook-Pro.local>
2023-07-23 20:42:02 +0600

    Update add & Edit button

--- frontend/app/components/userForm.jsx
+++ frontend/app/components/userForm.jsx
@@ -344,7 +344,13 @@ const UserForm = ({ type, params }) => {
 							type="submit"
 							className="Darkmode w-full md:w-max rounded-md bg-indigo-600 px-12 py-4 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 						>
-							SUBMIT
+							{type === "EDIT"
+								? updateUserMutation.isLoading
+									? "Updating..."
+									: "Update Now"
+								: createUserMutation.isLoading
+								? "Adding User ..."
+								: "Add New User"}
 						</button>
 					</div>
 				</div>
